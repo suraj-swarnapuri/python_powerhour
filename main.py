730864e543c28fac716c3d1e782e154fde79c547
Suraj Swarnapuri <suraj-swarnapuri@github.com>
2022-02-03 13:19:59 -0600

    finish skeleton

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import models
 
+"""
+classes
+inheritence
+polymorphism
+encapsulation
+error handling
+"""
 
 toyoda = models.Car("toyoda")
 model3 = models.SportsCar("model3")
--- models/sportsCar.py
+++ models/sportsCar.py
@@ -1,7 +1,13 @@
 from .car import Car
 class SportsCar(Car):
+    """
+    inherits Cars properties
+    """
     def __init__(self,name):
         Car.__init__(self,name)
+    """
+    Own version of go
+    """
     def go(self):
         self._pos = self._pos*2
         
